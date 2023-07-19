4cc59697d40de46916be1e96d3ab0ee4b57d65ea
A Raiu <raiu.andrei@protonmail.com>
2023-07-19 22:39:32 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+API_URL = "https://reqres.in/api/"
+MONGO_USER = "mongo"
+MONGO_PORT = 6300
+MONGO_PASSWD = "-"
+MONGO_HOST = "-"
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWD}@{MONGO_HOST}:{MONGO_PORT}"
+
+SPOTIFY = {
+    "CLIENT_ID": "-",
+    "CLIENT_SECRET": "-",
+    "PLAYLSIT_ID": "-",
+}
